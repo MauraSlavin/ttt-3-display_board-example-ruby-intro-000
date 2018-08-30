0deaba1d6c1dc969fe0ca79ac2be9498df2ebdae
Maura Slavin <mauramslavin@gmail.com>
2018-08-30 02:34:29 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,14 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+blank_cell = "   "
+vertical_separator = "|"
+horizontal_line = "-----------"
+for i in 1..3 do
+  puts blank_cell + vertical_separator + blank_cell + vertical_separator + blank_cell
+  puts horizontal_line
+  i += 1
+  if i==2
+    puts blank_cell + vertical_separator + blank_cell + vertical_separator + blank_cell
+    break
+  end  # of if
+end  # display_board
